6601ac1feef67553b2286ebae6f3db039222281f
Moeko Ebisawa [LIFULL] <ebisawam@PCZ03500.local>
2019-07-03 08:39:25 +0900

    template

---
+++ components/_template.vue
@@ -0,0 +1,23 @@
+<template>
+</template>
+
+<script>
+// Store使う場合はmapgettersで呼び出し
+// DB使う場合はfirebase
+// import { mapGetters } from 'vuex'
+// import { db, auth } from '../plugins/firebase'
+export default {
+    data () {
+        return {
+        }
+    },
+    computed: {
+    },
+    mounted () {
+    },
+    methods: {
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
