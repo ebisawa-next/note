9bae02341aa4ac0355b29c1a7569f30cc22f7e36
Moeko Ebisawa [LIFULL] <ebisawam@PCZ03500.local>
2019-07-03 09:07:42 +0900

    Update template

--- components/_template.vue
+++ components/_template.vue
@@ -5,11 +5,16 @@
 // Store使う場合はmapgettersで呼び出し
 // DB使う場合はfirebase
 // import { mapGetters } from 'vuex'
-// import { db, auth } from '../plugins/firebase'
+// import { db, auth } from '@/plugins/firebase'
 export default {
+    components: {
+
+    },
     data () {
         return {
         }
+    },
+    props: {
     },
     computed: {
     },
