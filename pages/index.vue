fa21f01e388c55c5236e22b83a23d6491fe95eba
Moeko Ebisawa [LIFULL] <ebisawam@PCZ03500.local>
2019-07-17 20:09:40 +0900

    index整頓

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,6 @@
 <template>
   <div>
     <Hero />
-    <p class="login" @click="googleSignIn">ログイン</p>
-    <section class="container" style="display: none">  
-      <div v-if="isSignedIn">
-          <p>ようこそ{{ userdata.name }}</p>
-          <nuxt-link to="/mypage">マイページに行こう</nuxt-link>
-      </div>
-      <p v-else class="login" @click="googleSignIn">ログイン</p>
-      <h1>くそフォーム</h1>
-      <div class="forms">
-        <input type="text" v-model="newNote" class="form" @keyup.enter="saveNote" placeholder="ほげほげほげ" />
-        <p class="sendButton" @click="saveNote">送る</p>
-      </div>
-        <h1>くそにっき</h1>
-        <p v-if="isSignedIn">{{ userdata.name }}のくそにっき</p>
-        <p v-else>名無し</p>
-        <ul>
-          <li v-for="(note, index) in notes" :key="index">
-            {{ note.content }}
-          </li>
-        </ul>
-    </section>
   </div>
 </template>
 
@@ -42,69 +21,15 @@ export default {
   },
   computed: {
     ...mapGetters({
-      notes: 'getNotes',
-      isSignedIn: 'users/getSignStatus',
-      userdata: 'users/getUserdata'
     }),
   },
   mounted () {
-    this.$store.dispatch('setNotesRef', db.collection('notes'))
   },
   methods: {
-    saveNote () {
-      if (!this.newNote.length) {
-        return
-      }
-      const newNote = {
-        content: this.newNote,
-        name: this.user ? this.user.displayName : 'Anonymous'
-      }
-      this.$store.dispatch('saveNote', newNote);
-      this.newNote = ''
-    },
-    googleSignIn () {
-      this.$store.dispatch('users/googleSignIn');
-    },
-    googleSignOut () {
-      this.$store.dispatch('users/googleSignOut');
-    },
   },
 }
 </script>
 
 <style lang="scss" scoped>
-.container {
-    width: 80%;
-    margin: 0 auto;
-    padding: 50px 0;
-}
-
-.forms {
-    display: flex;
-    margin: 10px 0 30px;
-
-    .form {
-        padding: 10px;
-        border-radius: 4px;
-        border: 1px solid #d8d8d8;
-    }
-
-    .sendButton {
-        background-color: #ed6103;
-        color: #fff;
-        padding: 10px;
-        border-radius: 4px;
-        margin-left: 10px;
-    }
-}
-
-.login {
-    padding: 10px;
-    background-color: #ffab00;
-    border-radius: 4px;
-    display: inline-flex;
-}
-
-
 </style>
 
